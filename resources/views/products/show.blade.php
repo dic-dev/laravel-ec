d0f944dbde30927a60c5977e06b4fb72d97fcb16
dic-dev <yamaguchi8246@gmail.com>
2023-09-17 11:42:08 +0900

    test

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <img src="{{ asset('storage/fishing.png') }}" alt="image" />
+            <img src="{{ asset('images/fishing.png') }}" alt="image" />
             <dl>
                 <dt>{{ $product->name }}</dt>
                 <dd>{{ $product->category->name }}</dd>
